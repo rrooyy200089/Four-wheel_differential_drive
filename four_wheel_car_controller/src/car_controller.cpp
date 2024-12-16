6e4298aa458482a36e4a53ccc6732bcb47907776
M11212044 <M11212044@gemail.yuntech.edu.tw>
2024-12-16 16:53:05 +0800

    修改里程計計算的方式與發布頻率，以及轉彎時的程度，並增加發布車子電池電壓的資訊

--- four_wheel_car_controller/src/car_controller.cpp
+++ four_wheel_car_controller/src/car_controller.cpp
@@ -5,6 +5,7 @@
 #include <serial/serial.h>
 #include <iostream>
 #include "std_msgs/String.h"
+#include "std_msgs/Float64.h"
 #include "geometry_msgs/Twist.h"
 #include <nav_msgs/Odometry.h>
 #include <tf/transform_broadcaster.h>
@@ -13,11 +14,11 @@ using namespace std;
 //创建一个serial类
 serial::Serial sp;
 
-#define  to_rad  0.017453f  //角度转弧度
+#define  to_rad  0.01745329f  //角度转弧度
 
 
 uint8_t FLAG_USART; //串口发送标志
-uint16_t count_1,count_2;//计数器
+uint16_t count_1,count_2, count_3;//计数器
 
 uint8_t Flag_start=0;//下位机运行标志
 float x_mid_speed; //
@@ -31,8 +32,10 @@ float Data_US[12];//发送到下位机的数据数组
 float Data_UR[22];//接收来自下位机的数据数组
 uint16_t a,b;
 void send_data(void);//串口发送协议函数
+uint8_t Flag_OK=0;
 
-// ros::Time last_cmdvelcb_time;
+double Roll = 0.0;    double Pitch = 0.0;    double Yaw = 0.0; double Yaw_zero = 0.0;double Yaw_error = 0.0; double Yaw_last = 0.0;
+
 bool new_message_received = false;
 int rate;
 string topic_odom, topic_cmd_vel;
@@ -142,18 +145,18 @@ void chatterCallback(const geometry_msgs::Twist::ConstPtr &msg)//获取键盘控
 			        }//右自传
 
        else if(x_mid_speed>0 && z_mid_speed==0 && z_mid_angle>0){//按下 U 键
-			               speed_A= x_mid_speed;
-				       speed_B= x_mid_speed*0.8F; //0.7F 
-				       speed_C= x_mid_speed*0.6F; //0.5F
-				       speed_D= x_mid_speed*0.97F;  //0.95F
+			               speed_A= x_mid_speed*1.1F;
+				       speed_B= x_mid_speed*0.9F; //0.7F 
+				       speed_C= x_mid_speed*0.7F; //0.5F
+				       speed_D= x_mid_speed*1.0F;  //0.95F
                       //  printf("u");  
 
 			        }//左斜上
        else if(x_mid_speed>0 && z_mid_speed==0 && z_mid_angle<0){//按下 O 键
-			               speed_B= x_mid_speed;
-				       speed_A= x_mid_speed*0.8F; 
-				       speed_D= x_mid_speed*0.6F;
-				       speed_C= x_mid_speed*0.97F; 
+			               speed_B= x_mid_speed*1.1F;
+				       speed_A= x_mid_speed*0.9F; 
+				       speed_D= x_mid_speed*0.7F;
+				       speed_C= x_mid_speed*1.0F; 
                       //  printf("o");				 
 
 			        }//右斜上
@@ -200,6 +203,8 @@ int main(int argc, char **argv){
     ros::init(argc, argv, "odometry_publisher");
     ros::NodeHandle n;
     ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>(topic_odom, 50);
+    ros::Publisher power_voltage_pub = n.advertise<std_msgs::Float64>("car_voltage", 10);
+    std_msgs::Float64 voltage;
 
     tf::TransformBroadcaster odom_broadcaster;
 
@@ -269,8 +274,9 @@ int main(int argc, char **argv){
 
    while(ros::ok())
    {
-    new_message_received = false;
-         float angular_velocity_x = Data_UR[1]*0.001064;//角速度转换成 rad/s
+     new_message_received = false;
+     if(Flag_OK==1){
+     float angular_velocity_x = Data_UR[1]*0.001064;//角速度转换成 rad/s
 	 float angular_velocity_y = Data_UR[2]*0.001064;//角速度转换成 rad/s
 	 float angular_velocity_z = Data_UR[3]*0.001064;//角速度转换成 rad/s			 
 	 float accelerated_speed_x = Data_UR[4]/16384;//转换成 g	,重力加速度定义为1g, 等于9.8米每平方秒
@@ -291,6 +297,10 @@ int main(int argc, char **argv){
 		
          float Power_D_X =	+Data_UR[11] ;    //D轮X方向速度
          float Power_D_Y =	0 ;    //A轮Y方向速度	
+
+         Roll = Data_UR[13];
+         Pitch = Data_UR[14];
+         Yaw = Data_UR[7];
     	
     
           vx  = (Power_A_X + Power_B_X + Power_C_X + Power_D_X)/4 ;//底盘当前X方向线速度 m/s	
@@ -302,19 +312,24 @@ int main(int argc, char **argv){
           //以给定机器人速度的典型方式计算里程计
           double dt = (current_time - last_time).toSec();
 
-          double delta_x = (vx * cos(th) - vy * sin(th)) * dt;
+          double delta_x = (vx * cos(Yaw_error*to_rad) - vy * sin(Yaw_error*to_rad)) * dt;
 
-          double delta_y = (vx * sin(th) + vy * cos(th)) * dt;
+          double delta_y = (vx * sin(Yaw_error*to_rad) + vy * cos(Yaw_error*to_rad)) * dt;
 
-          double delta_th = vth * dt;
+        //   double delta_th = vth * dt;
 
- 
+
+        static uint8_t only = 0;
+        if(only == 0) Yaw_zero = Yaw, only = 1; //每次运行记录一次初始角度作为零点
+
+        Yaw_error = Yaw - Yaw_zero; //当前角度值减去初始角度值
 
             x += delta_x;//X轴速度累积位移 m
 
             y += delta_y;//Y轴速度累积位移 m
 
-            th += delta_th;//Z轴角速度累积求车体朝向角度  rad //存在漂移
+            // th += delta_th;//Z轴角速度累积求车体朝向角度  rad //存在漂移
+            th = Yaw_error*to_rad;
 
 
       //因为所有的里程表都是6自由度的，所以我们需要一个由偏航创建的四元数
@@ -374,6 +389,9 @@ int main(int argc, char **argv){
        //发布消息
        odom_pub.publish(odom);
 
+       last_time = current_time;//保存为上次时间
+     }
+
         ros::spinOnce();//执行回调处理函数，完后继续往下执行
 				
 
@@ -404,7 +422,7 @@ int main(int argc, char **argv){
 		    }
             else if(!new_message_received){  //當沒有收到/cmd_vel新的訊息以後，就停止運動
                 count_2++;
-                if(count_2 > 25){
+                if(count_2 > 50){
                   /*<01>*/Data_US[0]  = Flag_start;//电机启动开关，1启动 0停止
                   /*<02>*/Data_US[1]  = 0; 
                   /*<03>*/Data_US[2]  = 0; 
@@ -420,7 +438,6 @@ int main(int argc, char **argv){
                   send_data();
                 }	
             }
-            last_time = current_time;//保存为上次时间
 
 		
           //获取下位机的数据				
@@ -447,7 +464,7 @@ int main(int argc, char **argv){
                        uint8_t sum=0; 
 	               for(uint8_t j=0;j<63;j++)sum+=buffer[j];    //计算校验和	
                        if(buffer[63] == (uint8_t)sum+buffer[0])
-                          {b++;	
+                          {b++;	Flag_OK=1;
                            for(uint8_t i=0;i<15;i++)//15个数据
                             {
 			     Data_UR[i] = b2f( buffer[4*i+3], buffer[4*i+4], buffer[4*i+5], buffer[4*i+6] );
@@ -476,7 +493,7 @@ int main(int argc, char **argv){
 			/*<14>*///Data_UR[14]; //预留
 
 		  count_1++;
-                  if(count_1>3){//显示频率降低为10HZ
+                  if(count_1>6){//显示频率降低为10HZ
                       count_1=0;
                       if((uint8_t)Data_UR[0]==1){
                           ROS_INFO("[00] Flag_start: [%u ]", (uint8_t)Data_UR[0]);ROS_INFO("[00] Flag_start: ON");}//下位机电机启动/停止标志，1启动，0停止
@@ -491,7 +508,7 @@ int main(int argc, char **argv){
                       ROS_INFO("[05] accel_y: [%d ]",  (int)Data_UR[5]); //Y轴加速度原始数据 accel_x
                       ROS_INFO("[06] accel_z: [%d ]",  (int)Data_UR[6]); //Z轴加速度原始数据 accel_x 				 
 
-                      ROS_INFO("[07] Yaw: [%.2f deg]",  Data_UR[7]); //Z轴角度 deg
+                      ROS_INFO("[07] Yaw: [%.2f deg]",  Data_UR[7]); //绕Z轴角度 deg
 													
                       ROS_INFO("[08] Current_linear_A: [%.2f m/s]", +Data_UR[8]); //A轮线速度 m/s
                       ROS_INFO("[09] Current_linear_B: [%.2f m/s]", -Data_UR[9]); //B轮线速度 m/s 
@@ -499,14 +516,23 @@ int main(int argc, char **argv){
                       ROS_INFO("[11] Current_linear_D: [%.2f m/s]", +Data_UR[11]); //D轮线速度 m/s 	 							
 								
                       ROS_INFO("[12] Voltage: [%.2f V]", Data_UR[12]/100); // 电池电压
-                      ROS_INFO("-----------------------"); 
+                      ROS_INFO("[13] Roll: [%.2f deg]", Data_UR[13]); //绕X轴角度 deg
+                      ROS_INFO("[14] Pitch: [%.2f deg]", Data_UR[14]); //绕Y轴角度 deg													
+													
                       ROS_INFO("a: [%d ]",   a);
                       ROS_INFO("b: [%d ]",   b);                       
 											ROS_INFO("a/b: [%.2f ]",   (float)a/(float)b);
+                      ROS_INFO("-----------------------"); 
                       if(b>5000)b=b/10,a=a/10;											
                      }					 
 
-
+        count_3++;
+        if(count_3 > 20){
+          int number = round(Data_UR[12]);
+          voltage.data = (double)number/100;
+          power_voltage_pub.publish(voltage);
+          count_3=0;
+        }
         loop_rate.sleep();//循环延时时间
    }
     
